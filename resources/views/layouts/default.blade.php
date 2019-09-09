9adeead3f5966e000dd36d54db9db67fe434996f
Theresa <s3580240@student.rmit.edu.au>
2019-09-09 18:31:00 +1000

    init scss

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,19 @@
 <html>
   <head>
     <title>@yield('title','StockY App')</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
+     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="/">StockY App</a>
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item"><a class="nav-link" href="/help">Help</a></li>
+          <li class="nav-item" ><a class="nav-link" href="#">Login</a></li>
+        </ul>
+      </div>
+    </nav>
+
     @yield('content')
   </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.default')
 @section('content')
-<h1>Home Page</h1>
+ <div class="jumbotron">
+    <h1>Home Page</h1>
+    <p class="lead">
+      Now you are seen <a href="https://learnku.com/courses/laravel-essential-training">Laravel </a> sample Home Page
+    </p>
+    <p>
+      Everything will begin from here
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">Login first</a>
+    </p>
+  </div>
 @stop
